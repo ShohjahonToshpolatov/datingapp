9a7cdbe701b0f0f3bf6dcdde3a27045d1a7da7b9
Shohjahon <sizning_emailingiz@example.com>
2025-11-23 18:21:07 +0500

    ErrorCore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  http = inject(HttpClient)
+  users: any;
+
+  ngOnInit(): void {
+    this.getUsers()
+  }
+
+  getUsers() {
+    this.http.get<any[]>('http://localhost:5251/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request completed')
+    })
+  }
 }
