711ead7bd4b3fe5055f32db1633871920502fbce
Shohjahon <sizning_emailingiz@example.com>
2025-12-08 20:23:57 +0500

    end of section 6

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using API.Data;
-using API.DTOs;
-using API.Entities;
-using API.Interfaces;
+using API.Data;          // DataContext uchun
+using API.DTOs;          // RegisterDto, LoginDto, UserDto
+using API.Entities;      // AppUser entity
+using API.Interfaces;    // ITokenService
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,7 +27,11 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+            if (string.IsNullOrWhiteSpace(registerDto.Username) || string.IsNullOrWhiteSpace(registerDto.Password))
+                return BadRequest("Username va password bo‘sh bo‘lishi mumkin emas");
+
+            if (await UserExists(registerDto.Username))
+                return BadRequest("Username is taken");
 
             using var hmac = new HMACSHA512();
 
@@ -41,38 +45,42 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return new UserDto
+            var userDto = new UserDto
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user)
             };
+
+            return Ok(userDto);
         }
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
+            if (string.IsNullOrWhiteSpace(loginDto.Username) || string.IsNullOrWhiteSpace(loginDto.Password))
+                return BadRequest("Username va password bo‘sh bo‘lishi mumkin emas");
+
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
-            if (user is null) return Unauthorized("Invalid username");
+            if (user is null) return BadRequest("Username mavjud emas");
 
-            if (user.PasswordSalt == null || user.PasswordHash == null) return Unauthorized("Invalid password");
+            if (user.PasswordSalt == null || user.PasswordHash == null) return BadRequest("Password noto‘g‘ri");
 
             using var hmac = new HMACSHA512(user.PasswordSalt);
-
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            if (user.PasswordHash.Length != computedHash.Length) return Unauthorized("Invalid password");
 
             for (int i = 0; i < computedHash.Length; i++)
             {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+                if (computedHash[i] != user.PasswordHash[i]) return BadRequest("Password noto‘g‘ri");
             }
 
-            return new UserDto
+            var userDto = new UserDto
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user)
             };
+
+            return Ok(userDto);
         }
 
         private async Task<bool> UserExists(string username)
@@ -81,4 +89,3 @@ namespace API.Controllers
         }
     }
 }
-
